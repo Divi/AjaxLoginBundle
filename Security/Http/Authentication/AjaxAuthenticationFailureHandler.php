7a7fb0504bd2641b9e014322bb1bf76e5c68a874
Divi <sylvain.lorinet@gmail.com>
2013-07-14 23:04:51 +0200

    Implement translation on error.

--- Security/Http/Authentication/AjaxAuthenticationFailureHandler.php
+++ Security/Http/Authentication/AjaxAuthenticationFailureHandler.php
@@ -2,16 +2,35 @@
 
 namespace Divi\AjaxLoginBundle\Security\Http\Authentication;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler;
+use Symfony\Component\Security\Http\HttpUtils;
 
 /**
  * @author Sylvain Lorinet <sylvain.lorinet@gmail.com>
  */
 class AjaxAuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
 {
+	private $translator;
+
+	/**
+	 * @param \Symfony\Component\HttpKernel\HttpKernelInterface $httpKernel
+	 * @param \Symfony\Component\Security\Http\HttpUtils $httpUtils
+	 * @param array $options
+	 * @param \Psr\Log\LoggerInterface $logger
+	 * @param type $translator
+	 */
+	public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, array $options, LoggerInterface $logger = null, $translator = null)
+	{
+		parent::__construct($httpKernel, $httpUtils, $options, $logger);
+
+		$this->translator = $translator;
+	}
+
 	/**
      * {@inheritDoc}
      */
@@ -20,7 +39,7 @@ class AjaxAuthenticationFailureHandler extends DefaultAuthenticationFailureHandl
 		if ($request->isXmlHttpRequest()) {
 			$json = array(
 				'has_error'	=> true,
-				'error'     => $exception->getMessage()
+				'error'     => $this->translator->trans($exception->getMessage())
 			);
 
 			return new Response(json_encode($json));
