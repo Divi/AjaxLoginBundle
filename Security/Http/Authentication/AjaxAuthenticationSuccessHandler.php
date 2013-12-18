dd46c7d63aa399bdb9a804808ad5d52d7dc1d663
matts2cant <matts2cant@gmail.com>
2013-12-18 15:29:08 +0100

    Fix Json Response in AjaxAuthenticationSuccessHandler

--- Security/Http/Authentication/AjaxAuthenticationSuccessHandler.php
+++ Security/Http/Authentication/AjaxAuthenticationSuccessHandler.php
@@ -3,7 +3,7 @@
 namespace Divi\AjaxLoginBundle\Security\Http\Authentication;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
 
@@ -17,14 +17,12 @@ class AjaxAuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandl
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
-		if ($request->isXmlHttpRequest()) {
-			$json = array(
-				'has_error'	  => false,
-				'username'	  => $token->getUser()->getUsername(),
-				'target_path' => $this->determineTargetUrl($request)
-			);
-
-			return new Response(json_encode($json));
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse(array(
+                'has_error'	  => false,
+                'username'	  => $token->getUser()->getUsername(),
+                'target_path' => $this->determineTargetUrl($request)
+    		));
 		}
 
 		return parent::onAuthenticationSuccess($request, $token);
